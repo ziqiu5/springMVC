6c25a38774d60c4c41f0266a1575cb18d0d7df0a
ziqiu <2775333199@qq.com>
2023-01-14 19:13:27 +0800

    second-commit

---
+++ module_springmvc03/src/test/java/com/atguigu/springMVC/test/Test01.java
@@ -0,0 +1,21 @@
+package com.atguigu.springMVC.test;
+
+import org.junit.Test;
+
+/**
+ * ClassName: Test01
+ * Package: com.atguigu.springMVC.test
+ * Description:
+ *
+ * @author ziqiu
+ * @Create: 2023/1/14 - 19:09  19:09
+ * @Version: v1.0
+ */
+
+
+public class Test01 {
+    @Test
+    public void test01(){
+        System.out.println("update!");
+    }
+}
